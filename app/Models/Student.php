653a207f722b3abee41255163ac8bf4e2bbb05b4
naruto1031 <naruto0614@au.com>
2023-12-27 17:43:38 +0900

    feature: 生徒情報管理機能の実装&操作を監視できるように設定

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,10 +6,30 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Student extends Model
+class Student extends Model implements Auditable
 {
 	use HasFactory;
+	use \OwenIt\Auditing\Auditable;
+
+	public function transformAudit(array $data): array
+	{
+		$data['user_id'] = request()->attributes->get('jwt_sub');
+		// TODO: 誰が変更したかを格納する
+		$data['user_type'] = 'student';
+		return $data;
+	}
+
+	protected $primaryKey = 'student_id';
+	protected $fillable = [
+		'student_id',
+		'class_id',
+		'email',
+		'name',
+		'registration_number',
+		'attendance_number',
+	];
 
 	public function schoolClass(): BelongsTo
 	{
