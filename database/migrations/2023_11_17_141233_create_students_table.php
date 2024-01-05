ed500d43d78029c9e54b417190125cea40c74217
naruto1031 <naruto0614@au.com>
2024-01-05 22:02:47 +0900

    急遽修正

--- database/migrations/2023_11_17_141233_create_students_table.php
+++ database/migrations/2023_11_17_141233_create_students_table.php
@@ -11,6 +11,7 @@ return new class extends Migration {
 	public function up(): void
 	{
 		Schema::create('students', function (Blueprint $table) {
+			$table->dropPrimary();
 			$table->string('student_id')->primary();
 			$table->unsignedBigInteger('class_id');
 			$table->string('email');
--- database/migrations/2023_11_17_141304_create_teachers_table.php
+++ database/migrations/2023_11_17_141304_create_teachers_table.php
@@ -11,6 +11,7 @@ return new class extends Migration {
 	public function up(): void
 	{
 		Schema::create('teachers', function (Blueprint $table) {
+			$table->dropPrimary();
 			$table->string('teacher_id')->primary();
 			$table->string('name');
 			$table->string('email');
